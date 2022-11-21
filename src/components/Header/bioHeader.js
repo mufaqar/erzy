96f164a84623bfe51ade365739899495716423ed
Mufaqar Islam <mufaqar@gmail.com>
2022-11-21 14:39:47 +0500

    Logo

--- src/components/Header/bioHeader.js
+++ src/components/Header/bioHeader.js
@@ -1,14 +1,16 @@
-import React from 'react'
-import {BHeader} from './HeaderStyles'
-import profile from '../../../public/images/Benefits4.png'
-import Image from 'next/image'
-
+import React from 'react';
+import { BHeader } from './HeaderStyles';
+import profile from '../../../public/images/Benefits4.png';
+import logo from '../../../public/images/smallLogo.png';
+import Image from 'next/image';
 
 export default function BioHeader() {
   return (
     <BHeader>
-      Header
-      <Image src={profile} alt='profile' width={90} height={90} />
+      <div>
+        <img src={logo} alt="profile" />
+      </div>
+      <Image src={profile} alt="profile" width={90} height={90} />
     </BHeader>
-  )
+  );
 }
